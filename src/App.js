fe6719a4bd263975a6161af6659f3616c395a20d
Monish Thakur <monisht@MPTDC1-India.MirrorPlus.com>
2023-01-21 23:51:34 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className='App-title'>welcome to react</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>bye there</h1>
+          <h2>welcome to the world of coding & competition</h2>
         </p>
         <a
           className="App-link"
